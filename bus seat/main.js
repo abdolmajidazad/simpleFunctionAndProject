7a7b84aba3f5b5540291ff0933ab8f45c720ad89
آزاد <dev3@jhoobin.local>
2018-10-24 13:09:35 +0330

    change bus seat

--- bus seat/main.js
+++ bus seat/main.js
@@ -1,52 +1,80 @@
 var settings = {
-    rows: 5,
-    // activeRow: [1, 1, false, false, -1],
-    activeRow: [1, 1, false, 0, 0],
-    get activeRowLength() {
-        return this.activeRow.filter(i => typeof i === 'number').length
-    },
+    // rows: 5,
+    // // activeRow: [1, 1, false, false, -1],
+    // activeRow: [1, 1, false, 0, 0],
+    // get activeRowLength() {
+    //     return this.activeRow.filter(i => typeof i === 'number').length
+    // },
+
+    // seat : [
+    //     [1,4,7,10,0,14,17,20,23],
+    //     [2,5,8,11,0,15,18,21,24],
+    //     [],
+    //     [],
+    //     [3,6,9,12,13,16,19,22,25]
+    // ],
+    seat : [
+        [1,5,9 ,13,17,0 ,0 ,25,29,33,37],
+        [2,6,10,14,18,0 ,0 ,26,30,34,38],
+        [],
+        [3,7,11,15,19,21,23,27,31,35,39],
+        [4,8,12,16,20,22,24,28,32,36,40]
+    ],
     cols: 15,
     rowCssPrefix: 'row-',
     colCssPrefix: 'col-',
-    seatWidth: 35,
-    seatHeight: 35,
+    seatWidth: 40,
+    seatHeight: 40,
     seatCss: 'seat',
     selectedSeatManCss: 'selectedSeatMan',
     selectedSeatWomanCss: 'selectedSeatWoman',
-    selectingSeatCss: 'selectingSeat'
+    selectingSeatCss: 'selectingSeat',
+    noneDisplay:'noneDisplay'
 };
 
 var init = function (reservedSeat) {
-    var str = [], seatNo, className;
-    for (i = 0; i < settings.rows; i++) {
-        for (j = 0; j < settings.cols; j++) {
-            if (typeof settings['activeRow'][i] === 'number') {
-                seatNo = (i + j * settings.activeRowLength + settings['activeRow'][i]);
-                className = settings.seatCss + ' ' + settings.rowCssPrefix + i.toString() + ' ' + settings.colCssPrefix + j.toString();
-                // if ($.isArray(reservedSeat) && $.inArray(seatNo, reservedSeat) != -1) {
-
-                let seatData = reservedSeat.find(i=>i['seatNumber']===seatNo)
+    var str = [],  className;
+    settings['seat'].forEach((list, index)=>{
+        list.forEach((seatNo,jIndex)=>{
+            className = '';
+            if(seatNo){
+                className = settings.seatCss + ' ' + settings.rowCssPrefix + index.toString() + ' ' + settings.colCssPrefix + jIndex.toString();
+                let seatData = reservedSeat.find(i=>i['seatNumber']===seatNo);
                 if (reservedSeat.length && typeof seatData === 'object') {
-                    // console.log("reservedSeat.find(i=>i===seatNo)::", reservedSeat.find(i=>i['seatNumber']===seatNo))
-                    // className += ' ' + settings.selectedSeatManCss;
-                    className += seatData['sex'] === 'male' ? ' '+settings.selectedSeatManCss : ' '+settings.selectedSeatWomanCss;
-                    console.log("className:", className )
+                    if(seatData['sex'] === 'male'){
+                        className += ' '+settings.selectedSeatManCss;
+                    }else if (seatData['sex'] === 'female'){
+                        className +=' '+settings.selectedSeatWomanCss;
+                    }
                 }
-                str.push('<li class="' + className + '"' +
-                    'style="top:' + (i * settings.seatHeight).toString() + 'px;left:' + (j * settings.seatWidth).toString() + 'px">' +
-                    '<a title="' + seatNo + '">' + seatNo + '</a>' +
-                    '</li>');
+            }else{
+                className +=' '+settings.noneDisplay;
             }
-
-        }
-    }
+            str.push('<li data="'+seatNo+'" class="' + className + '"' +
+                'style="top:' + (index * settings.seatHeight).toString() + 'px;left:' + (jIndex * settings.seatWidth).toString() + 'px">' +
+                // '<div class="seatInfo"><a title="' + seatNo + '">' + seatNo + '</a></div>' +
+                '</li>');
+        })
+    });
+    // for (i = 0; i < settings.rows; i++) {
+    //     for (j = 0; j < settings.cols; j++) {
+    //         if (typeof settings['activeRow'][i] === 'number') {
+    //             seatNo = (i + j * settings.activeRowLength + settings['activeRow'][i]);
+    //             className = settings.seatCss + ' ' + settings.rowCssPrefix + i.toString() + ' ' + settings.colCssPrefix + j.toString();
+    //             let seatData = reservedSeat.find(i=>i['seatNumber']===seatNo)
+    //             if (reservedSeat.length && typeof seatData === 'object') {
+    //                 className += seatData['sex'] === 'male' ? ' '+settings.selectedSeatManCss : ' '+settings.selectedSeatWomanCss;
+    //             }
+    //             str.push('<li class="' + className + '"' +
+    //                 'style="top:' + (i * settings.seatHeight).toString() + 'px;left:' + (j * settings.seatWidth).toString() + 'px">' +
+    //                 '<a title="' + seatNo + '">' + seatNo + '</a>' +
+    //                 '</li>');
+    //         }
+    //
+    //     }
+    // }
     $('#place').html(str.join(''));
 };
-//case I: Show from starting
-//init();
-
-//Case II: If already booked
-// var bookedSeats = [5, 10, 25];
 var bookedSeats = [
     {
         seatNumber: 5,
@@ -57,19 +85,26 @@ var bookedSeats = [
         sex: 'female'
     },
     {
-        seatNumber: 25,
+        seatNumber: 23,
         sex: 'male'
     }
 ];
 init(bookedSeats);
-
-
+let seatSelected = [];
 $('.' + settings.seatCss).click(function () {
     if ($(this).hasClass(settings.selectedSeatManCss) || $(this).hasClass(settings.selectedSeatWomanCss)) {
-        alert('This seat is already reserved');
+        alert('این صندلی قبلا رزرو شده است.');
     }
     else {
+
         $(this).toggleClass(settings.selectingSeatCss);
+        let seatNo = parseInt($(this).attr('data'));
+        if($(this).hasClass(settings.selectingSeatCss)){
+            seatSelected.push(seatNo);
+        }else{
+            seatSelected = seatSelected.filter(i=>i!==seatNo);
+        }
+        console.log("seatSelected:", seatSelected)
     }
 });
 
